677583e87cefdcbefbfe60261f847c91f8d36d90
sarnickaa <sarnicka.aleks@gmail.com>
2018-07-12 08:55:20 -0400

    3 tests failing
    unsure of what game logic requires - checking implementation

--- lib/wumpus/room.rb
+++ lib/wumpus/room.rb
@@ -3,7 +3,7 @@ module Wumpus
     def initialize(number)
       @number = number
       @hazards = []
-      @other_rooms = []
+      @neighbors = []
       # raise NotImplementedError, "See lib/wumpus/room.rb"
     end
 
@@ -31,23 +31,30 @@ module Wumpus
 
     def safe?
       # raise NotImplementedError, "See lib/wumpus/room.rb"
+      hazards.empty? && neighbors { |a| a.hazards.empty? }
     end
 
     def connect(other_room)
       # raise NotImplementedError, "See lib/wumpus/room.rb"
-      other_rooms.push(other_room)
+      neighbors.push(other_room)
+      # checked implementation code to figure this out
+      # room initialized with empty array - can push other rooms into it as exits
+      # to make the connection 2 way - must push current room into the neightbour array that is initialized when other_room is instatiated
+      # self refers to the OBJECT INSTANCE
+      other_room.neighbors.push(self)
+
     end
 
     def exits
-      # raise NotImplementedError, "See lib/wumpus/room.rb"
+      neighbors
     end
 
     def neighbor(number)
-      # raise NotImplementedError, "See lib/wumpus/room.rb"
+
     end
 
     def random_neighbor
-      # raise NotImplementedError, "See lib/wumpus/room.rb"
+      neighbors.sample
     end
   end
 end
